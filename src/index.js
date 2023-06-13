1fe188f8f3cc74000ad00d4259cb77965fe4d408
Shubham Joshi <sjoshi91119@gmail.com>
2023-06-13 12:49:24 +0530

    submitting code-Tue Jun 13 2023 12:49:22 GMT+0530 (India Standard Time)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+import React, { useState } from "react";
 
+function Tabs({ tabs }) {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+
+  return (
+    <div>
+      <ul>
+        {tabs.map((tab, index) => (
+          <li key={index} onClick={() => handleTabClick(index)}>
+            Tab {index + 1}
+          </li>
+        ))}
+      </ul>
+      <p>This is the content for Tab {activeTab + 1}</p>
+    </div>
+  );
+}
+
+export default Tabs;
 
 ReactDOM.render(<App />, document.getElementById("root"));
